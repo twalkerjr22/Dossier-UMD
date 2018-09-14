4096958cdbfe20f419a785bf312b62f08b500bc8
Troy W <twalkerjr22@gmail.com>
2018-09-14 11:36:10 -0400

    Removed App Client ID
    Removed the AWS App Client ID for Security

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
   cognito: {
     REGION: 'us-east-1',
     USER_POOL_ID: 'us-east-1_v8bBoAzHY',
-    APP_CLIENT_ID: '5vs9mbsp2mei7i1co1i0ubrfv8',
+    APP_CLIENT_ID: 'XXXXXXXXXXX',
     IDENTITY_POOL_ID: 'YOUR_IDENTITY_POOL_ID',
   },
 };
